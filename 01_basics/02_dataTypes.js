3da3e53088a95ce5a7d0dc7dbb7b495c1edd3ab1
ishansingh01 <ishansingh453@gmail.com>
2025-08-10 10:10:23 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";//treat all JS code as newer version
+
+// alert(3+3) // we are using node.js not browser
+
+//tc39.es and mdn for documentation
+console.log(3 + 
+    3 ) // always focus on high readiabilty 
+
+    console.log("Ishan");
+    let age = 19
+/* Premitive data types
+number => 2 to power 53 range 
+bigInt use for large number
+boolean => true 
+string =""
+null => standalone value
+undefined
+symbol=> unique
+*/
+
+//object
+
+
+console.log(typeof "Ishan");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
