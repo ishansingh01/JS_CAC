3178f14f8054d88a1c6c7b96cc64f1fd2e303899
ishansingh01 <ishansingh453@gmail.com>
2025-09-11 12:21:40 +0000

    string

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,9 +1,35 @@
-const name = "hitesh"
-const repoCount = 50
+// const name = "hitesh"
+// const repoCount = 50
 
-// console.log(name + " " +repoCount + " Value");
+// // console.log(name + " " +repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String ('hiteshhc')
-console.log(gameName);
+const gameName = new String ('hitesh-hc-com')
+
+
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf("t"));
+
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8,4);
+// console.log(anotherString);
+
+
+// const newStringOne = "     ishan   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://ishan.com/ishan%20singh"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('ishan'));
+
+console.log(gameName.split('-'));
