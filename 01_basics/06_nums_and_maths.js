7666eacb6867a7ffddb7e3633ddeb5a109489295
ishansingh01 <ishansingh453@gmail.com>
2025-09-13 21:04:09 +0000

    math_and_nums

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,39 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);//convert to string then give length of string. 
+// console.log(balance.toFixed(2));//result second digit.
+
+// const otherNumber = 123.4978
+// console.log(otherNumber.toPrecision(4));//result with roundoff 4th digit
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));//by default give us if we write en-IN convert indian rupees
+
+
+// ++++++++++++++MATHS++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-5));//turn negative value in positive
+// console.log(Math.round(4.3));//round off the number remove decimal digit
+// console.log(Math.ceil(4.2));//round to upper value
+// console.log(Math.floor(4.2));//round to lower value
+// console.log(Math.min(3,4,5,42,2,1));//min value in array
+// console.log(Math.max(3,4,5,42,2,1));//mmaximum value in array
+
+
+console.log(Math.random());//give random value between 0 to 1
+console.log((Math.random()*10)+1);//value always greater than 1
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max - min + 1)) + min)//give min 10
+
+
+
+
+
+
