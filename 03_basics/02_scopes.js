aba8881207fe9b61deb41b74aa9cc0f556a48508
ishansingh01 <ishansingh453@gmail.com>
2026-01-18 06:27:07 +0000

    02_scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -10,10 +10,50 @@ if(true){
 }
 
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
     
-}
-console.log(a);
+// }
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one() {
+    const username = "Ishan"
+
+    function two() {
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+    
+}
+
+// one()
+
+if (true) {
+    const username = "Ishan"
+    if(username === "Ishan"){
+        const website = " Youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// ******************* interresting *********************
+
+console.log(addone(5))
+function addone(num) {
+    return num + 1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
